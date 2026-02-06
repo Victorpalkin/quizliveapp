77a3c766408c1a7082ac84a7187d7146026c0c24
Viktor Palkin <palkin@google.com>
2026-02-06 09:43:18 +0000

    Removing app check enforcement TEMP

--- functions/src/utils/appCheck.ts
+++ functions/src/utils/appCheck.ts
@@ -16,7 +16,7 @@ import { HttpsError, CallableRequest } from 'firebase-functions/v2/https';
 
 // Check if App Check enforcement is enabled via environment variable
 // This allows gradual rollout: monitor first, then enforce
-const APP_CHECK_ENFORCEMENT = process.env.APP_CHECK_ENFORCEMENT === 'true';
+const APP_CHECK_ENFORCEMENT = process.env.APP_CHECK_ENFORCEMENT === 'false';
 
 /**
  * Verify App Check token from request
